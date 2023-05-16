23acb7b961ce8085f06004cfc9626659d21cf2be
dani <danifromecuador@gmail.com>
2023-05-16 18:57:15 -0500

    Add clear cart button

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,7 +1,9 @@
+import { clearCart } from "../features/cart/cartSlice";
 import CartItem from "./CartItem"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
   if (amount < 1) {
     return (
@@ -28,7 +30,7 @@ const CartContainer = () => {
       <div className="cart-total">
         <h4>total <span>${total}</span> </h4>
       </div>
-      <button className="btn clear-btn">clear cart</button>
+      <button className="btn clear-btn" onClick={() => dispatch(clearCart()) }>clear cart</button>
     </footer>
   </section>
 
