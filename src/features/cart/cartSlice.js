210b5dab82cdd203caae1ea29e7ca42ef9a7a466
dani <danifromecuador@gmail.com>
2023-05-16 19:38:52 -0500

    Create increase and decrease reducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -6,7 +6,7 @@ const initialState = {
   amount: 4,
   total: 0,
   isLoading: true,
-}
+} 
 
 const cartSlice = createSlice({
   name: "cart",
@@ -15,11 +15,23 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id!== itemId);
+    },
+    increase: (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount += 1;
+    },
+    decrease: (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount -= 1;
+    },
   },
 });
 
 // console.log(cartSlice);
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
